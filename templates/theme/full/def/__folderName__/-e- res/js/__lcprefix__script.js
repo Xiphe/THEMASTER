cc6f34637c2239c33c894a56a945b4fd5cae4f14
Hannes Diercks <xiphe@gmx.de>
2012-07-31 10:27:27 +0200

    adding resources to theme template

---
+++ templates/theme/full/def/__folderName__/-e- res/js/__lcprefix__script.js
@@ -0,0 +1,23 @@
+(function($){var
+	
+	/* PRIVATE VARS */
+	thiz = this
+
+	/* PUBLIC VARS */ ;
+
+
+	/* PRIVATE FUNCTIONS */ var
+
+	_init = function() {
+
+	},
+
+	_ready = function() {
+
+	}
+
+	/* PUBLIC FUNCTION */ ;
+
+
+// INITIAION ETC.
+;(function(){_init();$(document).ready(_ready);});})(jQuery);
