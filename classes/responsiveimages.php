4a857b53072b13263961b01a8bc78f8863bd37fb
Hannes Diercks <xiphe@gmx.de>
2012-09-16 22:46:52 +0200

    Added ResponsiveImages Class

---
+++ classes/responsiveimages.php
@@ -0,0 +1,360 @@
+<?php 
+namespace Xiphe\THEMASTER;
+
+class ResponsiveImages extends THEWPMASTER {
+	public $singleton = true;
+
+	protected $actions_ = array(
+		'wp_ajax_tm_responsiveimageget',
+		'wp_ajax_tm_responsiveimageget' => 'wp_ajax_nopriv_tm_responsiveimageget',
+		'wp_ajax_tm_responsivslideshowget',
+		'wp_ajax_tm_responsivslideshowget' => 'wp_ajax_nopriv_tm_responsivslideshowget',
+	);
+
+	public function init() {
+		$this->reg_js('resizeend');
+		$this->reg_js('tm-responsiveimages');
+		$this->reg_jsVar('ajaxurl', admin_url('admin-ajax.php'));
+	}
+
+	public function wp_ajax_tm_responsiveimageget() {
+		if (!isset($_REQUEST['nonce'])
+		 || !THEWPTOOLS::verify_noprivnonce(
+				esc_attr($_REQUEST['nonce']),
+				'tm-responsive',
+				esc_attr($_REQUEST['image'])
+			)
+		) {
+			$this->_exit('error', 'Authentication failed.', 1);
+		}
+
+		$this->_r['uri'] = $this->get_url(
+			esc_attr($_REQUEST['image']),
+			esc_attr($_REQUEST['width'])
+		);
+		$this->_exit('ok', 'URI is attached.', 0);
+	}
+
+	public function wp_ajax_tm_responsivslideshowget() {
+		if (!isset($_REQUEST['nonce'])
+		 || !THEWPTOOLS::verify_noprivnonce(
+				esc_attr($_REQUEST['nonce']),
+				'tm-responsive',
+				esc_attr($_REQUEST['image'])
+			)
+		) {
+			$this->_exit('error', 'Authentication failed.', 2);
+		}
+		$this->_r['img'] = trim($this->get_image(
+			esc_attr($_REQUEST['image']),
+			esc_attr($_REQUEST['width']),
+			(isset($_REQUEST['class']) ? esc_attr($_REQUEST['class']) : null),
+			(isset($_REQUEST['id']) ? esc_attr($_REQUEST['id']) : null),
+			(isset($_REQUEST['alt']) ? esc_attr($_REQUEST['alt']) : null),
+			(isset($_REQUEST['title']) ? esc_attr($_REQUEST['title']) : null)
+		));
+
+		$this->_exit('ok', 'Image is attached', 0);
+	}
+
+	public function get_url($image, $width = 'auto')
+	{
+		if (!($image = $this->_get_baseImageFile($image))) {
+			return false;
+		}
+		$height = $this->_get_dims($image, $width);
+		return $this->_get_imageUrl($image, $width, $height);
+	}
+
+
+	public function get_bg_imageAttrs($image, $width = 'auto')
+	{
+		$slideshow = $this->_is_slideshow($image);
+
+		$origin = $image;
+		if (!($image = $this->_get_baseImageFile($image))) {
+			return false;
+		}
+
+		$height;
+		$loadWidth = $this->_get_loadWidh($image, $width, $height);
+
+		$url = $this->get_url(
+			$image, 
+			$loadWidth
+		);
+
+		return array(
+			'style' => "background-image: url('$url');",
+			'class' => 'tm-responsiveimage tm-responsivebgimage',
+			'data-width' => $width,
+			'data-height' => $height,
+			'data-ratio' => ($height/$width),
+			'data-origin' => $origin,
+			'data-loaded' => $loadWidth,
+			'data-template' => $this->_gen_imageUrlFrom(
+				$this->_build_imageFileName($image, ':w', ':h')
+			),
+			'data-nonce' => THEWPTOOLS::create_noprivnonce('tm-responsive', $origin),
+		);
+	}
+
+
+	public function get_image($image, $width = 'auto', $addClass = false, $addId = null, $alt = null, $title = null)
+	{
+		/*
+		 * Check if $image is array or object initiate slideshow and use first entry as startimage.
+		 */
+		$slideshow = $this->_is_slideshow($image);
+
+		/*
+		 * Check if alt-text is provided or image meta-alt is provided.
+		 */
+		if ($alt === null && $image == intval($image)) {
+			$alt = get_post_meta($image, '_wp_attachment_image_alt', true);
+		}
+
+		if ($title === null && $image == intval($image)) {
+			$title = get_the_title($image);
+		}
+
+		/*
+		 * Convert attachment ids into image paths and check if image file is existent.
+		 */
+		$origin = $image;
+		if (!($image = $this->_get_baseImageFile($image))) {
+			return false;
+		}
+
+		$height;
+		$loadWidth = $this->_get_loadWidh($image, $width, $height);
+
+		$url = $this->get_url(
+			$image, 
+			$loadWidth
+		);
+
+		$args = array(
+			'src' => $url,
+			'class' => trim('tm-responsiveimage'.(!empty($addClass) ? ' '.str_replace('tm-responsiveimage', '', $addClass) : '')),
+			'data-width' => $width,
+			'data-height' => $height,
+			'data-ratio' => ($height/$width),
+			'data-origin' => $origin,
+			'data-loaded' => $loadWidth,
+			'data-template' => $this->_gen_imageUrlFrom(
+				$this->_build_imageFileName($image, ':w', ':h')
+			),
+			'data-nonce' => THEWPTOOLS::create_noprivnonce('tm-responsive', $origin),
+			'width' => '100%',
+			'id' => $addId
+		);
+		if (isset($alt)) {
+			$args['alt'] = $alt;
+			$args['data-fixalt'] = true;
+		}
+		if (isset($title)) {
+			$args['title'] = $title;
+			$args['data-fixtitle'] = true;
+		}
+		if ($slideshow) {
+			$args['data-slideshow'] = implode(',', $slideshow);
+			$args['data-slidenonce'] = THEWPTOOLS::create_noprivnonce('tm-responsive', $args['data-slideshow']);
+		}
+		return THEBASE::sget_HTML()->r_img($args);
+	}
+
+	public function image($image, $width = 'auto', $addClass = false, $addId = null, $alt = null, $title = null) {
+		echo $this->get_image($image, $width, $addClass, $addId, $alt, $title);
+	}
+
+	private function _get_loadWidh($image, &$width, &$height)
+	{
+		/*
+		 * Check if image should be delivered in full size directly.
+		 * (drct) prefix before actual with.
+		 */
+		$direct = false;
+		if (substr($width, 0, 4) == 'drct') {
+			$direct = true;
+			$width = intval(str_replace('drct', '', $width));
+		}
+
+		/*
+		 * Get the potential dimensions.
+		 */		
+		$height = $this->_get_dims($image, $width, false);
+
+		/*
+		 * Get the url of mini-thumb or direct full image if drct prefix was set.
+		 */
+		if ($direct) {
+			$loadWidth = $width;
+			$this->_get_dims($image, $loadWidth);
+		} else {
+			$loadWidth = 50;
+		}
+		return $loadWidth;		
+	}
+
+	private function _is_slideshow(&$image)
+	{
+		$slideshow = false;
+		if (is_string($image)) {
+			$image = explode(',', $image);
+		}
+		if (count($image) > 1) {
+			$slideshow = array();
+			foreach ($image as $k => $img) {
+				if (!isset($theimg)) {
+					$theimg = $img;
+				} else {
+					$slideshow[] = $img;
+				}
+			}
+			$image = $theimg;
+			if (empty($slideshow)) {
+				$slideshow = false;
+			} else {
+				$slideshow[] = $image;
+			}
+		} else {
+			$image = $image[0];
+		}
+		return $slideshow;
+	}
+
+	private function _get_imageUrl($image, $width, $height)
+	{
+		return $this->_gen_imageUrlFrom(
+			$this->_get_imageFile($image, $width, $height)
+		);
+	}
+
+	private function _gen_imageUrlFrom($file)
+	{
+		return preg_replace(
+			'/[\/\\\\]/',
+			'/',
+			get_bloginfo('wpurl').'\\'.str_replace(
+				ABSPATH,
+				'',
+				$file
+			)
+		);
+	}
+
+	private function _get_imageFile($image, $width, $height)
+	{
+		$file = $this->_build_imageFileName($image, $width, $height);
+		if (!file_exists($file)) {
+			$this->_gen_image($image, $file, $width, $height);
+		}
+		return $file;
+	}
+
+	private function _build_imageFileName($image, $width, $height) {
+		return dirname($image).DS.'tm-responsive'.DS.pathinfo($image, PATHINFO_FILENAME)
+			.'-'.$width.'x'.$height.'.'.pathinfo($image, PATHINFO_EXTENSION);
+	}
+
+	private function _gen_image($original, $target, $width, $height)
+	{
+		$type = wp_check_filetype($original);
+		$type = $type['type'];
+
+		switch ($type) {
+			case 'image/gif':
+				$original = imagecreatefromgif($original);
+				break;
+			case 'image/png':
+				$original = imagecreatefrompng($original);
+				break;
+			default:
+				$original = imagecreatefromjpeg($original);
+				break;
+		}
+		$new_image = imagecreatetruecolor($width, $height);
+
+		if ($type == 'image/png' || $type == 'image/gif') {
+			imagealphablending($new_image, false);
+			imagesavealpha($new_image,true);
+			$transparent = imagecolorallocatealpha($new_image, 255, 255, 255, 127);
+			imagefilledrectangle($new_image, 0, 0, $width, $height, $transparent);
+		}
+
+      	imagecopyresampled(
+      		$new_image,
+      		$original, 
+      		0, 0, 0, 0,
+      		$width,
+      		$height,
+      		imagesx($original),
+      		imagesy($original)
+      	);
+
+      	if (!is_dir(dirname($target))) {
+      		mkdir(dirname($target));
+      	}
+      	switch ($type) {
+			case 'image/gif':
+				$r = imagegif($new_image, $target);
+				break;
+			case 'image/png':
+				$r = imagepng($new_image, $target);
+				break;
+			default:
+				$r = imagejpeg($new_image, $target, 80);
+				break;
+		}
+		imagedestroy($original);
+		imagedestroy($new_image);
+		return $r;
+	}
+
+	private function _get_dims($image, &$width, $round = true)
+	{
+		$dims = getimagesize($image);
+		if ($width == 'auto') {
+			$width = $dims[0];
+			return $dims[1];
+		}
+
+		if ($round) {
+			if($width < 200) {
+				$rnd = 50;
+			} elseif($width < 1000) {
+				$rnd = 100;
+			} else {
+				$rnd = 200;
+			}
+			$width = ceil(intval($width)/$rnd)*$rnd;
+		}
+		if ($width > $dims[0]) {
+			$width = $dims[0];
+			return $dims[1];
+		}
+		
+		return round($width/($dims[0]/$dims[1]));
+	}
+
+	private function _get_baseImageFile($image) {
+		if (!is_int($image)) {
+			if (file_exists($image)) {
+				return $image;
+			} 
+			$image = intval($image);
+		}
+		$image = wp_get_attachment_metadata($image);
+		if (!is_array($image)) {
+			return false;
+		}
+		$ft = wp_check_filetype($image['file']);
+		$fts = explode('/', $ft['type']);
+		if ($fts[0] != 'image') {
+			return false;
+		} 
+		return WP_CONTENT_DIR.DS.'uploads'.DS.$image['file'];
+	}
+}
+?>
---
+++ res/js/tm-responsiveimages.js
@@ -0,0 +1,152 @@
+jQuery(document).ready(function($) {
+
+	var slideimgs = {},
+		sTime = 5000;
+
+
+
+	var responsize = function(elm) {
+		if ($(elm).hasClass('tm-responsiveimage')) {
+			innerresponsize.call(elm);
+		} else {
+			$.each($('.tm-responsiveimage'), function() {
+				innerresponsize.call(this);
+			});
+		}
+	},
+
+	innerresponsize = function() {
+		var rnd;
+		if ($(this).width() < 200) {
+			rnd = 50;
+		} else if ($(this).width() < 1000) {
+			rnd = 100;
+		} else {
+			rnd = 200;
+		}
+		var thiz = this,
+			nW = Math.ceil($(this).width()/rnd)*rnd;
+
+		if(parseInt($(this).attr('data-loaded'), 10) !== nW) {
+			var	nH = Math.round(nW*$(this).attr('data-ratio')),
+				url = $(this).attr('data-template').replace(':h', nH).replace(':w', nW),
+				$img = $('<img />');
+
+			$img.load(function() {
+				setImg.call(thiz, url, nW);
+			}).error(function() {
+				$.get(ajaxurl, {
+					action: 'tm_responsiveimageget',
+					width: $(thiz).width(),
+					image: $(thiz).attr('data-origin'),
+					nonce: $(thiz).attr('data-nonce')
+				}, function(r) {
+					if (r && r.status === 'ok') {
+						setImg.call(thiz, url, nW);
+					}
+				});
+			})[0].src = url;
+		}
+
+		var sldshw = $(this).attr('data-slideshow');
+		if (typeof sldshw !== 'undefined' && sldshw !== false && sldshw !== '') {
+			window.setTimeout(function() {
+				slideshow.call(thiz);
+			}, 0);
+		}
+	},
+
+	setImg = function(url, size) {
+		if ($(this).hasClass('tm-responsivebgimage')) {
+			$(this).css({'background-image' : 'url('+url+')'});
+		} else {
+			$(this).attr('src', url);
+		}
+		$(this).attr('data-loaded', size);
+	},
+
+	slideshow = function() {
+		var thiz = this,
+			addit = false,
+			$img = false;
+
+		if (typeof slideimgs[$(this).attr('data-slideshow')] === 'undefined') {
+			var args = {
+					action:  'tm_responsivslideshowget',
+					width:   'drct'+$(this).width(),
+					image:   $(this).attr('data-slideshow'),
+					nonce:   $(this).attr('data-slidenonce'),
+					id:      $(this).attr('id'),
+					'class': $(this).attr('class'),
+				};
+			if ($(this).attr('data-fixalt')) {
+				args.alt = $(this).attr('alt');
+			}
+			if ($(this).attr('data-fixtitle')) {
+				args.title = $(this).attr('title');
+			}
+
+			$.get(ajaxurl, args, function(r) {
+				r = eval("(" + r + ")");
+				if(r && r.status === 'ok') {
+					$(r.img).load(function() {
+						slideimgs[args.image] = r.img;
+						if (addit === true) {
+							slideshowNext.call(thiz, $(this));
+						} else {
+							$img = $(this);
+						}
+					});
+				}
+			});		
+		} else {
+			$img = $(slideimgs[$(this).attr('data-slideshow')]);
+		}
+		
+		window.setTimeout(function() {
+			if ($img !== false) {
+				slideshowNext.call(thiz, $img);
+			} else {
+				addit = true;
+			}
+		}, sTime);
+	},
+
+	slideshowNext = function($img) {
+		var $old = $(this);
+		$old = $old.wrap('<div />').parent().css({
+			'position' : 'relative',
+			'z-index' : 0
+		});
+		$img.wrap('<div />').parent().css({
+			'height' : 0,
+			'opacity' : 0,
+			'position' : 'relative',
+			'z-index' : 1
+		});
+		$old.before($img.parent());
+		$img.parent().animate({'opacity' : '1'}, 1000, function() {
+			$old.remove();
+			$img.unwrap();
+			responsize($img[0]);
+		});
+	},
+
+	resize = function() {
+		$.each($('.tm-responsiveimage'), function() {
+			$(this).attr('height', Math.round($(this).width()*$(this).attr('data-ratio')));
+		});
+	};
+	
+	(function() {
+		if(typeof tm_slideshowTime !== 'undefined') {
+			sTime = parseInt(tm_slideshowTime);
+		}
+		resize();
+		window.setTimeout(function() {
+			responsize();
+			$(window).resize(resize);
+			$(window).resizeEnd(responsize);
+		}, 10);
+	})();
+});
